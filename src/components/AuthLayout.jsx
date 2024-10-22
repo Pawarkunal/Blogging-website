88923b0624aad5328d0c3fdda64ceae5cdc48eab
pawarkunal <pawarkunal.m@gmail.com>
2024-10-22 10:57:24 +0530

    blog project

---
+++ src/components/AuthLayout.jsx
@@ -0,0 +1,24 @@
+// it is mechanism the way pages and routes are protecting. by creating container. it is protected container. here will decide wheather to reder it or not.
+
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+export default function Protected({ children, authentication = true }) {
+  const navigate = useNavigate();
+  const [loader, setLoader] = useState(true);
+  const authStatus = useSelector((state) => state.auth.status);
+
+  useEffect(() => {
+    // TODO: make it more easy to uderstand
+    if (authentication && authStatus !== authentication) {
+      navigate("/login");
+    } else if (!authentication && authStatus !== authentication) {
+      navigate("/");
+    }
+    setLoader(false);
+  }, [authStatus, navigate, authentication]);
+
+  
+  return loader ? <h1>Loading...</h1> : <>{children}</>;
+}
---
+++ src/components/index.js
@@ -0,0 +1,32 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/Logoutbtn";
+import Input from "./Input";
+import Button from "./Button";
+import RTE from "./RTE";
+import AuthLayout from "./AuthLayout";
+import Select from "./Select";
+import Signup from "./Signup";
+import Login from "./Login";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
+
+
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    Input,
+    Button,
+    RTE,
+    AuthLayout,
+    Select,
+    Signup,
+    Login,
+    PostForm,
+    PostCard,
+}
